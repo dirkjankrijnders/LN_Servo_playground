0e68066f4ebc55bf4dd26887e0a6d5122c970a21
Dirkjan Krijnders <dirkjankrijnders@incas3.eu>
2016-02-16 08:19:03 +0100

    Moved the configured pin classes to their own files, from the loconet decoder sketch

---
+++ configuredpins.cpp
@@ -0,0 +1,78 @@
+#include <configuredpins.h>
+#include <Arduino.h>
+#include <LocoNet.h>
+#ifndef DEBUG
+#define DEBUG(x)
+#endif
+
+    InputPin::InputPin(uint8_t confpin, uint8_t pin, uint16_t address) : ConfiguredPin(confpin, pin, address){pinMode(_pin, INPUT_PULLUP); state = 0; laststate = 1;};
+    void InputPin::print() {DEBUG("Input pin ");DEBUG(_pin);DEBUG("\n");};
+    void InputPin::update() {
+      state = digitalRead(_pin);
+      //DEBUG(state);
+      if (state != laststate) {
+        LocoNet.reportSensor(_address, state);
+        DEBUG("State pin ");
+        DEBUG(_pin);
+        DEBUG(" changed to ");
+        DEBUG(state);
+        laststate = state;
+      }
+    }
+  
+ServoSwitch::ServoSwitch(uint8_t confpin, uint8_t pin, uint16_t address) : ConfiguredPin(confpin, pin, address){};
+ServoSwitch::ServoSwitch(uint8_t confpin, uint8_t pin, uint16_t address, uint16_t pos1, uint16_t pos2, uint16_t speed) :ConfiguredPin(confpin, pin, address) { 
+  _straight = pos1;
+  _turnout = pos2;
+  _speed = speed;
+  _servo.attach(pin);
+  };
+
+void ServoSwitch::changepin(uint8_t pin) {_servo.detach(); _pin = pin; _servo.attach(pin);};
+
+void ServoSwitch::set(bool dir, bool state) {
+  if (dir){
+    _servo.writeMicroseconds(_turnout);
+  } else {
+    _servo.writeMicroseconds(_straight);
+  }
+  LocoNet.reportSwitch(_address);
+  _state = dir;
+};
+
+void ServoSwitch::toggle() {
+  if (_state) {
+    _servo.writeMicroseconds(_straight);
+    _state = !_state;
+  } else {
+    _servo.writeMicroseconds(_turnout);
+    _state = !_state;
+  }
+};
+
+void ServoSwitch::update () {
+};
+
+void ServoSwitch::print(){
+  
+  DEBUG("Arduino Pin: ");
+  DEBUG(_pin);
+  DEBUG(" Straight: ");
+  DEBUG(_straight);
+  DEBUG(" Turnout: ");
+  DEBUG(_turnout);
+  DEBUG(" Address: ");
+  DEBUG(_address);
+  DEBUG(" Speed: ");
+  DEBUG(_speed);
+  DEBUG("\n");
+};
+
+void ServoSwitch::print_state() {
+  if (_state) {
+    DEBUG("Straight");
+  } else {
+    DEBUG("Thrown");
+  }
+  DEBUG(_servo.read());
+};
---
+++ configuredpins.h
@@ -0,0 +1,48 @@
+#ifndef CONFIGURED_PINS_H
+#define CONFIGURED_PINS_H
+
+#include <Servo.h>
+
+class ConfiguredPin{
+  public:
+    ConfiguredPin(uint8_t confpin, uint8_t pin, uint16_t address) {_confpin = confpin; _pin = pin, _address = address;};
+    virtual void print() {};
+    virtual void toggle() {};
+    virtual void set(bool port, bool state) {};
+    virtual void print_state() {};
+    virtual void update() {};
+  public:
+    uint8_t _pin;
+    uint16_t _address;
+  private:
+    uint8_t _confpin;
+};
+
+class InputPin : public ConfiguredPin {
+  public:
+    InputPin(uint8_t confpin, uint8_t pin, uint16_t address);
+    void print();
+    void update();
+    bool state;
+    bool laststate;
+};
+  
+class ServoSwitch : public ConfiguredPin {
+  public:
+    ServoSwitch(uint8_t confpin, uint8_t pin, uint16_t address);
+    ServoSwitch(uint8_t confpin, uint8_t pin, uint16_t address, uint16_t pos1, uint16_t pos2, uint16_t speed);
+    void changepin(uint8_t pin);
+    void set(bool dir, bool state);
+    void toggle();
+    void update ();
+    void print();
+    void print_state();
+    uint16_t _turnout;
+    uint16_t _straight;
+    uint16_t _speed;
+    bool _state;
+    Servo _servo;
+  private:
+};
+
+#endif
