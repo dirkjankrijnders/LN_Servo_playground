76ad1a73251594c721c938678d1c40d5a823431e
Dirkjan Krijnders <dirkjan@krijnders.net>
2016-04-30 13:16:47 +0200

    More save defaults

--- default_conf.h
+++ default_conf.h
@@ -3,32 +3,32 @@
 3,						//   3, Unused
 4,						//   4, Unused
 5,						//   5, Unused
-6,						//   6, Number of pins configured
+0,						//   6, Number of pins configured
 7,						//   7, Manufacturer
 1, 						//   8, Version
-(1 << PIN_CONF_INPUT),	//   9, Pin as input, i.e. least chance of breaking anything
-(1 << PIN_CONF_INPUT),	//  10, Pin as input, i.e. least chance of breaking anything
-(1 << PIN_CONF_INPUT),	//  11, Pin as input, i.e. least chance of breaking anything
-(1 << PIN_CONF_INPUT),	//  12, Pin as input, i.e. least chance of breaking anything
-(1 << PIN_CONF_INPUT),	//  13, Pin as input, i.e. least chance of breaking anything
-(1 << PIN_CONF_INPUT),	//  14, Pin as input, i.e. least chance of breaking anything
-(1 << PIN_CONF_INPUT),	//  15, Pin as input, i.e. least chance of breaking anything
-(1 << PIN_CONF_INPUT),	//  16, Pin as input, i.e. least chance of breaking anything
-(1 << PIN_CONF_INPUT),	//  17, Pin as input, i.e. least chance of breaking anything
-(1 << PIN_CONF_INPUT),	//  18, Pin as input, i.e. least chance of breaking anything
-(1 << PIN_CONF_INPUT),	//  19, Pin as input, i.e. least chance of breaking anything
-(1 << PIN_CONF_INPUT),	//  20, Pin as input, i.e. least chance of breaking anything
-(1 << PIN_CONF_INPUT),	//  21, Pin as input, i.e. least chance of breaking anything
-(1 << PIN_CONF_INPUT),	//  22, Pin as input, i.e. least chance of breaking anything
-(1 << PIN_CONF_INPUT),	//  23, Pin as input, i.e. least chance of breaking anything
-(1 << PIN_CONF_INPUT),	//  24, Pin as input, i.e. least chance of breaking anything
-(1 << PIN_CONF_INPUT),	//  25, Pin as input, i.e. least chance of breaking anything
-(1 << PIN_CONF_INPUT),	//  26, Pin as input, i.e. least chance of breaking anything
-(1 << PIN_CONF_INPUT),	//  27, Pin as input, i.e. least chance of breaking anything
-(1 << PIN_CONF_INPUT),	//  28, Pin as input, i.e. least chance of breaking anything
-(1 << PIN_CONF_INPUT),	//  29, Pin as input, i.e. least chance of breaking anything
-(1 << PIN_CONF_INPUT),	//  30, Pin as input, i.e. least chance of breaking anything
-(1 << PIN_CONF_INPUT),	//  31, Pin as input, i.e. least chance of breaking anything
+(0 << PIN_CONF_INPUT),	//   9, Pin as undefined, i.e. least chance of breaking anything
+(0 << PIN_CONF_INPUT),	//  10, Pin as undefined, i.e. least chance of breaking anything
+(0 << PIN_CONF_INPUT),	//  11, Pin as undefined, i.e. least chance of breaking anything
+(0 << PIN_CONF_INPUT),	//  12, Pin as undefined, i.e. least chance of breaking anything
+(0 << PIN_CONF_INPUT),	//  13, Pin as undefined, i.e. least chance of breaking anything
+(0 << PIN_CONF_INPUT),	//  14, Pin as undefined, i.e. least chance of breaking anything
+(0 << PIN_CONF_INPUT),	//  15, Pin as undefined, i.e. least chance of breaking anything
+(0 << PIN_CONF_INPUT),	//  16, Pin as undefined, i.e. least chance of breaking anything
+(0 << PIN_CONF_INPUT),	//  17, Pin as undefined, i.e. least chance of breaking anything
+(0 << PIN_CONF_INPUT),	//  18, Pin as undefined, i.e. least chance of breaking anything
+(0 << PIN_CONF_INPUT),	//  19, Pin as undefined, i.e. least chance of breaking anything
+(0 << PIN_CONF_INPUT),	//  20, Pin as undefined, i.e. least chance of breaking anything
+(0 << PIN_CONF_INPUT),	//  21, Pin as undefined, i.e. least chance of breaking anything
+(0 << PIN_CONF_INPUT),	//  22, Pin as undefined, i.e. least chance of breaking anything
+(0 << PIN_CONF_INPUT),	//  23, Pin as undefined, i.e. least chance of breaking anything
+(0 << PIN_CONF_INPUT),	//  24, Pin as undefined, i.e. least chance of breaking anything
+(0 << PIN_CONF_INPUT),	//  25, Pin as undefined, i.e. least chance of breaking anything
+(0 << PIN_CONF_INPUT),	//  26, Pin as undefined, i.e. least chance of breaking anything
+(0 << PIN_CONF_INPUT),	//  27, Pin as undefined, i.e. least chance of breaking anything
+(0 << PIN_CONF_INPUT),	//  28, Pin as undefined, i.e. least chance of breaking anything
+(0 << PIN_CONF_INPUT),	//  29, Pin as undefined, i.e. least chance of breaking anything
+(0 << PIN_CONF_INPUT),	//  30, Pin as undefined, i.e. least chance of breaking anything
+(0 << PIN_CONF_INPUT),	//  31, Pin as undefined, i.e. least chance of breaking anything
 32,
 33,
 34,
