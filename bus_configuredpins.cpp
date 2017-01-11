572637a74021cf022d2c16a0e42841f04be112c7
Dirkjan Krijnders <dirkjan@krijnders.net>
2017-01-11 11:26:48 +0100

    Fixed reorder warning

--- bus_configuredpins.cpp
+++ bus_configuredpins.cpp
@@ -1,6 +1,6 @@
 #include "bus_configuredpins.h"
 
-TLC5947pin::TLC5947pin(Adafruit_TLC5947* _tlc, uint8_t confpin, uint8_t pin, uint16_t address, bool cumulative, uint8_t _channel, uint16_t _intensity) : OutputPin(confpin, pin, address, cumulative), tlc(_tlc),  channel(_channel), intensity(_intensity) {
+TLC5947pin::TLC5947pin(Adafruit_TLC5947* _tlc, uint8_t confpin, uint8_t pin, uint16_t address, bool cumulative, uint8_t _channel, uint16_t _intensity) : OutputPin(confpin, pin, address, cumulative), tlc(_tlc), intensity(_intensity), channel(_channel) {
 };
 
 void TLC5947pin::_set(bool state) {
